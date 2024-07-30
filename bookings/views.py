31dfe6c4213fc92b0b7394501580eb036114a967
Sunita Arora <sunita_arora@live.com>
2024-07-30 12:37:16 +0000

    removed about app added bookings app

--- bookings/views.py
+++ bookings/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
-"""
-Displays home page"
-"""
-    template_name = 'index.html'
+    """
+    Displays home page"
+    """
+    template_name = 'index.html' 
--- kids_first_aid/settings.py
+++ kids_first_aid/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
-    'about',
+    'bookings',
 
 ]
 

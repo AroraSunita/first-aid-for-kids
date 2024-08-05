a3c3fbfe7494fbbf65f08c4a3418c91aaa565636
Sunita Arora <sunita_arora@live.com>
2024-08-05 13:42:55 +0000

    in base.html, added Logout, Register and Login links

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Contact, Course, Booking
 
 # Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('comment', 'date')
+ 
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'first_name', 'last_name', 'email', 'course', 'date', 'time')
+    search_fields = ('user__username', 'course__name', 'email')
+    list_filter = ('date', 'course')
+    

8b9e1041272cc9aa464e6beede8b3f82ed6d33e8
Sunita Arora <sunita_arora@live.com>
2024-08-07 10:47:40 +0000

    added coursed in the database, courses showing up and started with booking.html

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from .models import Contact
+from .models import Contact, Booking
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+
 
 class ContactForm(forms.ModelForm):
     class Meta:
@@ -15,4 +16,15 @@ class ContactForm(forms.ModelForm):
         self.helper.template_pack = 'bootstrap5'
         self.helper.add_input(Submit('submit', 'Submit'))
 
- 
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['first_name', 'last_name', 'email', 'course', 'date', 'time', 'comment']
+
+    def __init__(self, *args, **kwargs):
+        super(BookingForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.template_pack = 'bootstrap5'
+        self.helper.add_input(Submit('submit', 'Book'))
--- bookings/views.py
+++ bookings/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
 from django.contrib import messages
-from .forms import ContactForm
-from .models import Course
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from .forms import ContactForm, BookingForm
+from .models import Course, Booking
+
 
 class IndexView(View):
     def get(self, request):
@@ -17,7 +20,34 @@ class IndexView(View):
             return redirect('home')
         return render(request, 'bookings/index.html', {'form': form})
 
+
+
 class CoursesView(View):
     def get(self, request):
-        # courses = Course.objects.all()  # Retrieve all course objects from database
-        return render(request, 'bookings/course.html')
+        courses = Course.objects.all()  # Retrieve all course objects from database
+        return render(request, 'bookings/course.html', {'courses': courses})
+
+
+
+# @method_decorator(login_required, name='dispatch')
+# class BookingView(View):
+#     def get(self, request, course_id):
+#         course = get_object_or_404(Course, id=course_id)
+#         form = BookingForm(initial={
+#             'course': course,
+#             'email': request.user.email,
+#             'first_name': request.user.first_name,
+#             'last_name': request.user.last_name
+#         })
+#         return render(request, 'bookings/booking.html', {'form': form, 'course': course})
+
+#     def post(self, request, course_id):
+#         course = get_object_or_404(Course, id=course_id)
+#         form = BookingForm(request.POST)
+#         if form.is_valid():
+#             booking = form.save(commit=False)
+#             booking.user = request.user
+#             booking.save()
+#             messages.success(request, 'Great, you have successfully booked the course!')
+#             return redirect('home')
+#         return render(request, 'bookings/booking.html', {'form': form, 'course': course})

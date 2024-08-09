8c78a55fbb7dd162ea1c6261a3ecc8e636b3a16c
Sunita Arora <sunita_arora@live.com>
2024-08-09 18:27:03 +0000

    updated the navbar signed in  user able to see booked courses and alert message style

--- bookings/models.py
+++ bookings/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 # Create your models here.
 
 class Contact(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
     email = models.CharField()
     comment = models.TextField(blank=True)
     date = models.DateField(default=datetime.now, blank=True)
@@ -14,20 +14,21 @@ class Contact(models.Model):
         return f"Contact form {self.name}"
 
 class Course(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=50)
     description =  models.TextField(max_length=20000, blank=True)
+    image = models.ImageField(upload_to='course_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
-    first_name = models.CharField(max_length=200, blank=True, null=True)
-    last_name = models.CharField(max_length=200, blank=True, null=True)
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
     email = models.CharField()
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     date = models.DateField(default=datetime.now, blank=True)
-    time = models.CharField(max_length=200, default= '10:00 - 11:00')
+    time = models.CharField(max_length=20, default= '09:00 - 10:00')
     comment = models.TextField(blank=True)
 
     def __str__(self):
